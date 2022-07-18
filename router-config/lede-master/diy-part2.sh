b05ddff069ad23406bbc68d336b795a912b65ef3
yusuf13278 <109559111+yusuf13278@users.noreply.github.com>
2022-07-19 06:16:54 +0700

    Update diy-part2.sh

--- router-config/lede-master/diy-part2.sh
+++ router-config/lede-master/diy-part2.sh
@@ -33,13 +33,13 @@ sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999
 
 # ------------------------------- Other started -------------------------------
 #
-# Add luci-app-amlogic
+ Add luci-app-amlogic
 svn co https://github.com/ophub/luci-app-amlogic/trunk/luci-app-amlogic package/luci-app-amlogic
 
 # Add luci-app-passwall
 # svn co https://github.com/xiaorouji/openwrt-passwall/trunk package/openwrt-passwall
 
-# Add luci-app-openclash
+ Add luci-app-openclash
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/openwrt-openclash
 pushd package/openwrt-openclash/tools/po2lmo && make && sudo make install 2>/dev/null && popd
 
